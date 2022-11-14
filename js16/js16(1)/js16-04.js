f95f5725f5b97c568df27b6ef754c9917c24a657
leesibin <tlqls123@gmail.com>
2022-11-14 17:49:17 +0900

    오후 수업 11/14

--- js16/js16(1)/js16-04.js
+++ js16/js16(1)/js16-04.js
@@ -1,14 +1,13 @@
-    setTimeout(()=>{
-            alert('펑!')
-            },6001)
-    
-        function fn(){              
-            let count = 6;
-     stop1 =  setInterval(()=>{
-            --count
-            console.log(`${count}번째`)
-        },1000)
-    }
+var count = 5
+        function fn(){
+            setInterval(function(){
+                if(count>0)
+                    console.log(count--)
+                else
+                    console.log('펑!')
+                    return
+            },1000)
+            }
     function fnx(){
     setTimeout(()=>{ 
             clearInterval(stop1)   
