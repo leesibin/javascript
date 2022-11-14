abcb9df68b9a78e3e36e2004c344475b4547edd6
leesibin <tlqls123@gmail.com>
2022-11-14 21:32:41 +0900

    집 11/14

--- js17/js17-01.js
+++ js17/js17-01.js
@@ -1,13 +1,22 @@
-function fn(){
-    
-    let count = 0;
-   time = setInterval(()=>{
-        count ++;
-        console.log(`${count}`)
-    },1000)
+function fn() {
+
+    let sec = 55;
+    var min = 55;
+
+    time = setInterval(() => {
+        sec++
+        if (sec > 59) {
+            sec = 0;
+            min++;
+            if (min > 59) {
+                min = 0;
+            }
+        }
+        console.log(`${min}분${sec}초`)
+    }, 1000)
 }
-function fnx(){
+function fnx() {
     setTimeout(() => {
         clearInterval(time)
-    },1000);
+    }, 1000);
 }
