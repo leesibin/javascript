feca04117a01d4b1dd6f04c2c4934b235f6bbfbc
leesibin <tlqls123@gmail.com>
2022-11-24 17:42:55 +0900

    오후수업 11/24

--- js17/js17-01.js
+++ js17/js17-01.js
@@ -3,15 +3,11 @@ var sec = 55;
 var min = 0;
 function fn() {
     time = setInterval(() => {
-        sec++
-        if (sec > 59) {
-            sec = 0;
-            min++;
-            if (min > 59) {
-                min = 0;
-            }
-        }
-        console.log(`${min}분${sec}초`)
+       sec++; //1초마다 증가
+       set = min%60;
+       
+
+        document.write(`${min}분${sec}초`)
     }, 1000)
     return
 }
